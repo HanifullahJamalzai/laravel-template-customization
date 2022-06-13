4d6725a3306ba207ee59d561f0cd79269a974e65
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2022-06-13 17:40:27 +0430

    .

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -13,6 +13,7 @@ class LandingPageController extends Controller
         $services = Service::all();
         $title = 'Focus On What Matters';
         return view('landing.home')->with('title', $title)->with('services', $services);
+        
         // return view('landing.home', compact('title'));
         // return view('landing.home', ['title' => 'Focus On What Matters']);
     }
